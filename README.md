15e479ab4833228fce296fc7fc4dfeaf19f984c2
acgy <yves@macbookpro.com>
2021-04-12 00:03:05 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,86 @@
+# Flashforge Finder php API
+This is an unofficial php API for the Flashforge Finder inspired from https://github.com/01F0/flashforge-finder-api/. It allows to request status information from the printer like temperature, progress, etc. It may also work for other Flashforge printer models but has onls been tested on the Finder.
+
+## Warning
+Use this API at your own risk. It only performs reading operations but it is unofficial and may of course have bugs.
+
+## Installation
+
+This package is installed via [Composer](https://getcomposer.org/).
+
+Run the following to require the package via Composer
+```sh
+    composer require acgy/flashforge-finder-php-api
+```
+
+## Usage
+Instanciate the printer with IP address and port (port is optionnal, specify it only if you're not using the 9988 default port).
+
+Example output:
+
+```php
+$printer = new \acgy\Printer('192.168.0.5');
+print_r($printer->get('progress'));
+/*
+Array
+(
+    [1] => 543
+    [2] => 1000
+)
+*/
+```
+
+If you want to display the full text returned by the printer, use the verbose mode
+
+### Available commands (in verbose mode)
+```php
+echo $printer->get('control', true);
+/*
+CMD M601 Received.
+Control Success.
+ok
+*/
+
+echo $printer->get('info', true);
+/*
+CMD M115 Received.
+Machine Type: Flashforge Finder
+Machine Name: My 3D Printer
+Firmware: ***********
+SN: *******
+X: 140 Y: 140 Z: 140
+Tool Count:1
+ok
+*/
+
+echo $printer->get('position', true);
+/*
+CMD M114 Received.
+X:0 Y:0 Z:0 A:0 B:0
+ok
+*/
+
+echo $printer->get('temperature', true);
+/*
+CMD M105 Received.
+T0:210 /210 B:0/0
+ok
+*/
+
+echo $printer->get('progress', true);
+/*
+CMD M27 Received.
+SD printing byte 543/1000
+ok
+*/
+
+echo $printer->get('status', true);
+/*
+MD M119 Received.
+Endstop: X-max:1 Y-max:0 Z-max:1
+MachineStatus: READY
+MoveMode: READY
+Status: S:1 L:0 J:0 F:0
+ok
+*/
+```
---
+++ src/Printer/Printer.class.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace acgy\Printer;
+
+class Printer {
+    private $ip_address;
+    private $port;
+    private $request_messages = [
+        'control' => '~M601 S1\r\n',
+        'info' => '~M115\r\n',
+        'position' => '~M114\r\n',
+        'temperature' => '~M105\r\n',
+        'progress' => '~M27\r\n',
+        'status' => '~M119\r\n'
+    ];
+    private $request_regexs = [
+        'control' => '#(Control Success)#',
+        'info' => '',
+        'position' => '#X:(-?[0-9\.]+) Y:(-?[0-9\.]+) Z:(-?[0-9\.]+) A:(-?[0-9\.]+)#',
+        'temperature' => '#T0:([0-9]+) /([0-9]+)#',
+        'progress' => '#printing byte ([0-9]+)/([0-9]+)#',
+        'status' => '#MachineStatus: (.+)#'
+    ];
+
+    /**
+     * Constructor
+     *
+     * @param string $printer_address The printer IP address
+     * @param string $port The printer port, by default 8899
+     *
+     **/
+    function __construct($ip_address, $port = '8899') {
+        $this->ip_address = $ip_address;
+        $this->port = $port;
+    }
+
+    /**
+     * Sends a request to the printer a returns a string with the output
+     *
+     * @param string $message_data The request to send to the printer
+     * @return String
+     **/
+    public function send_and_receive($message_data){
+        // instanciating the socket
+        $socket = socket_create(AF_INET, SOCK_STREAM, 0);
+        if ($socket === false) {
+            echo "socket_create() failed: " . socket_strerror(socket_last_error()) . "\n";
+            return false;
+        }
+        // connecting to the printer
+        $connection = socket_connect ($socket, $this->ip_address, $this->port);
+        if ($connection === false) {
+            echo "Connection failed\n";
+            return false;
+        }
+        //stream_set_timeout ($socket, 5);
+        // send the request to the printer
+        $message = $message_data;
+        socket_send($socket, $message, strlen($message), 0);
+        // receiving the response from the printer
+        $response = "";
+        socket_recv ($socket, $response , 1024, 0);
+        // closing the socket
+        socket_close($socket);
+        // returning the response
+        return $response;
+    }
+
+    /**
+     * Requests an information to the printer
+     *
+     * @param string $request_message The information to get
+     * @return string Information
+     **/
+    public function get($request_message, $verbose = false)
+    {
+        // checking that the requested information is available in the requests messages
+        if (array_key_exists($request_message, $this->request_messages)){
+            $output = $this->send_and_receive($this->request_messages[$request_message]);
+            if ($verbose){
+                return $output;
+            }
+            // extracting the information from the output
+            else if (!empty($this->request_regexs[$request_message]) && preg_match($this->request_regexs[$request_message], $output, $matches)){
+                unset($matches[0]);
+                return $matches;
+            }
+            else return $output;
+        }
+        else {
+            echo "Request '".$request_message."' is not available";
+            return false;
+        }
+    }
+}
+?>
